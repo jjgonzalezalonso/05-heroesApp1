f1c2d8f62aad6b2f42668fbc3427a70391cbf620
Jorge Gonzalez <infjgonzalez@elorrieta-errekamari.com>
2023-11-10 08:48:22 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m =>m.HeroesModule)},
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m =>m.AuthModule)},
+  { path: '404', component: ErrorPageComponent },
+  { path: '**', redirectTo:'404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
